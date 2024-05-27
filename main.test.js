9c3c167988a011e97361bf1ac80be3ccdf438d41
iankmani <ian.kiarie2002@gmail.com>
2024-05-27 16:40:38 +0300

    add prettier files

--- main.test.js
+++ main.test.js
@@ -1,19 +1,17 @@
 // const { describe } = require("test");
 const { FizzBuzz } = require("./main");
 describe("FizzBuzz", () => {
-    it("should return FizzBuzz for multiples of 3 and 5", () => {
-        expect(FizzBuzz(15)).toBe("FizzBuzz");
-    });
-    it("should return Fizz for multiples of 3", () => {
-        expect(FizzBuzz(3)).toBe("Fizz");
-    });
-    it("should return Buzz for multiples of 5", () => {
-        expect(FizzBuzz(5)).toBe("Buzz");
-    });
-    it("should return the number for non-multiples", () => {
-        expect(FizzBuzz(1)).toBe(1);
-    });
+  it("should return FizzBuzz for multiples of 3 and 5", () => {
+    expect(FizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return Fizz for multiples of 3", () => {
+    expect(FizzBuzz(3)).toBe("Fizz");
+  });
+
+  it("should return Buzz for multiples of 5", () => {
+    expect(FizzBuzz(5)).toBe("Buzz");
+  });
+  it("should return the number for non-multiples", () => {
+    expect(FizzBuzz(1)).toBe(1);
+  });
 });
-
-
-
